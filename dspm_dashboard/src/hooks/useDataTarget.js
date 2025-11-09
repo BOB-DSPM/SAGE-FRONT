0a2eb439c55ee6dcdca668569e3e21918807c019
wonjjani <wonjjani@gmail.com>
2025-11-09 15:11:55 +0900

    Feat:haa

--- dspm_dashboard/src/hooks/useDataTarget.js
+++ dspm_dashboard/src/hooks/useDataTarget.js
@@ -15,7 +15,7 @@ export const useDataTarget = (activeTab) => {
       setLoadingInventory(true);
       setError(null);
       try {
-        const res = await fetch('http://211.44.183.248:8000/api/all-resources');
+        const res = await fetch('http://43.202.228.52:8000/api/all-resources');
         if (!res.ok) {
           setError(`API ${res.status} ${res.statusText}`);
           setInventoryData([]);
