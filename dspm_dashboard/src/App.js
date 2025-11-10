6b0022876b1121a81dcb73ea5647a96b00fd316a
wonjjani <wonjjani@gmail.com>
2025-11-10 15:03:09 +0900

    Fear:gha

--- dspm_dashboard/src/App.js
+++ dspm_dashboard/src/App.js
@@ -41,7 +41,7 @@ const tabs = [
   //{ id: 'alerts', name: 'Alerts', icon: Bell },
   // { id: 'policies', name: 'Compliance Result', icon: BarChart3 },
 
-  // { id: 'opensource', name: 'Opensource', icon: Boxes },
+  { id: 'opensource', name: 'Opensource', icon: Boxes },
 ];
 
 const DashboardLayout = ({ children, onLogout, showSidebar = true }) => {
